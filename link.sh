568351bac5de5b780d854c5b1247805fd1c380f7
ptlx <ptlx@outlook.jp>
2022-11-05 15:43:45 +0900

    create jetpack installation script

--- link.sh
+++ link.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+curl -fLo ~/.vim/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim --create-dirs https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim
 ln -snfv ~/dotfiles/.vimrc ~/.vimrc
