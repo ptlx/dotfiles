a5dedefac2af5913e4f9561579fd1f40c39cd9df
ptlx <ptlx@outlook.jp>
2022-11-05 17:54:53 +0900

    move files

--- nvim/link.sh
+++ nvim/link.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 curl -fLo ~/.local/share/nvim/site/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim --create-dirs https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim
-ln -snfv ~/dotfiles/init.lua ~/.config/nvim/init.lua
+ln -snfv ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
