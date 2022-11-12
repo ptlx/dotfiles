79d74a1916f5fba1f98ff834e8d5535c4547c1c8
ptlx <ptlx@outlook.jp>
2022-11-12 22:29:43 +0900

    add installer

---
+++ installer.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+DOT_DIR = "$HOME/dotfiles"
+
+has() {
+    type "$1" > /dev/null 2 > &1;
+}
+
+if [ ! -d ${DOT_DIR} ]; then
+    # clone 
+    if has "git"; then
+        git clone https://github.com/ptlx/dotfiles.git ${DOT_DIR}
+    else
+        echo "git is required"
+        exit 1
+    fi
+    cd ${DOT_DIR}
+    for f in *
+    do
+        [[ "$f" == ".git" ]] && continue
+        [[ "$f" == ".gitignore" ]] && continue
+        [[ "$f" == "README.md" ]] && continue
+        [[ "$f" == "install.sh" ]] && continue
+        ln -snf $DOT_DIR/"$f" $HOME/".$f"
+        echo "Installed .$f"
+    done
+else
+    echo "dotfiles already exists"
+fi
+
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+git -C $(brew --repo homebrew/core) checkout master
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# install requirements
+sudo apt update
+sudo apt install ripgrep -y
+
+# run link.sh in respective directory
+for f in *
+do
+    if [ -f $f ] ; then
+	# directory of installer files is excluded
+	[[ "$f" == "installers" ]] && continue
+
+	cd "${DOT_DIR}/${f}"
+	bash link.sh
+	
+
