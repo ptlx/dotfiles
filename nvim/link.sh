29ff383e2333b65fbccbcdbde6c2aed3ce5747e5
ptlx <ptlx@outlook.jp>
2023-01-08 13:53:25 +0000

    fix link

--- nvim/link.sh
+++ nvim/link.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -fLo ~/.local/share/nvim/site/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim --create-dirs https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim
 ln -snfv ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
+ln -snfv ~/dotfiles/nvim/lua ~/.config/nvim/lua
